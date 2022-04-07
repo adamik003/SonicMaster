0a0aa367321705593e7c2697e6f05dafbcdc1f46
JakNask <97445306+JakNask@users.noreply.github.com>
2022-04-07 10:37:40 +0200

    Update2

---
+++ NewBilet/NewBilet/Form4.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NewBilet
+{
+    public partial class Form4 : Form
+    {
+        public Form4()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            Form5 four = new Form5();
+            four.Show();
+            this.Hide();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            Form3 four = new Form3();
+            four.Show();
+            this.Hide();
+        }
+    }
+}
